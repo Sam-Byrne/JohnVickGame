03cfdbdd2c48ba5b4a59ef3ae0a20ee330ae7af7
Sam <d00277274@student.dkit.ie>
2025-10-25 16:51:36 +0100

    Movement and death anims
    Omnidirectional movement has been made and also the death animations

--- John Vick Survival/Assets/Scripts/Player.cs
+++ John Vick Survival/Assets/Scripts/Player.cs
@@ -4,7 +4,14 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed;
     float speedX, speedY;
+    
+    bool looking;
     Rigidbody2D rb;
+
+    Vector2 movement;
+
+    public Animator animator;
+    int direction = 1;
 
     void Start()
     {
@@ -14,9 +21,24 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        
         speedX = Input.GetAxisRaw("Horizontal");
         speedY = Input.GetAxisRaw("Vertical");
         rb.linearVelocity = new Vector2(speedX, speedY).normalized * moveSpeed;
+
+        animator.SetFloat("Horizontal", speedX);
+        animator.SetFloat("Vertical", speedY);
+        animator.SetFloat("Speed", speedX);
+        animator.SetInteger("Direction", direction);
+        
+        if (speedX > 0)
+        {
+            direction = 1;
+        }
+        else if (speedX < 0)
+        {
+            direction = -1;
+        }
         
     }
 }
