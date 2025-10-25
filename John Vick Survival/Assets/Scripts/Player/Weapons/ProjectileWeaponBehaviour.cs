d9730fd5c6377035a32ab4327271f4a1d448356e
Sam <d00277274@student.dkit.ie>
2025-10-25 23:50:26 +0100

    Working on the weapon

---
+++ John Vick Survival/Assets/Scripts/Player/Weapons/ProjectileWeaponBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ProjectileWeaponBehaviour : MonoBehaviour
+{
+
+    protected Vector3 direction;
+    public float destroyAfterSeconds;
+
+    protected virtual void Start()
+    {
+        Destroy(gameObject, destroyAfterSeconds);
+    }
+
+
+    public void DirectionChecker(Vector3 dir)
+    {
+        direction = dir;
+    }
+}
