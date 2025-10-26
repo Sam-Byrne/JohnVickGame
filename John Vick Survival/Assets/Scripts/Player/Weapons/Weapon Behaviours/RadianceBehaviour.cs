03b10d1d4f005830843f2bb00dd3364585a25e7d
Sam <d00277274@student.dkit.ie>
2025-10-26 16:36:24 +0000

    I imported a thing
    lots

---
+++ John Vick Survival/Assets/Scripts/Player/Weapons/Weapon Behaviours/RadianceBehaviour.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class RadianceBehaviour : MeleeWeaponBehaviour
+{
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+
+}
--- John Vick Survival/Assets/Scripts/Player/Weapons/Weapon Controllers/RadianceController.cs
+++ John Vick Survival/Assets/Scripts/Player/Weapons/Weapon Controllers/RadianceController.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class RadianceController : MonoBehaviour
+public class RadianceController : WeaponController 
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void Start()
     {
+        base.Start();
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        GameObject spawnedRadiance = Instantiate(prefab);
+        spawnedRadiance.transform.position = transform.position;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
